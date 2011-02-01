79220de1936f5ae5df867aab08456f6cbb00513c
Franz Liedke <franz@fluxbb.org>
2011-02-01 21:46:44 +0100

    Make tests look better in command line.

--- testcase/all_test.php
+++ testcase/all_test.php
@@ -18,7 +18,7 @@ foreach (new DirectoryIterator(getcwd()) as $entry) {
     if ($entry->isFile() && strpos($entry, '_testcase.')>0) {
         echo basename($entry);
         require_once($entry);
-        echo '<br>...pass!<br><br>';
+        echo '...pass!'."\n\n";
     }
 }
 ?>
